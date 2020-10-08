56d77cec5bfb1648d8396c321d161b25b6a61c75
abeltasfir <43387070+abeltasfir@users.noreply.github.com>
2020-10-08 17:06:29 +0700

    Update SSimage.js

--- SSimage.js
+++ SSimage.js
@@ -1,36 +1,5 @@
-'use strict';
+$(".mini img").click(function(){  
 
-function galleryDefault() {
-  largeImg.setAttribute('src', thumbs.children[0].getAttribute('href'));
-  largeImg.setAttribute('alt', thumbs.children[0].getAttribute('title'));
-};
+ $(".maxi").attr("src",$(this).attr("src").replace("100x100","400x400"));
 
-function gallerySort(event) {
-  var target = event.target;
-
-  function selectedImg(select, alt) {
-    largeImg.setAttribute('src', select);
-    largeImg.setAttribute('alt', alt);
-    event.preventDefault();
-  };
-
-  while (target != this) {
-
-    if (target.nodeName == 'A') {
-      var select = target.getAttribute('href');
-      var alt = target.getAttribute('title');
-      return selectedImg(select, alt);
-    };
-
-    target = target.parentNode;
-  };
-
-};
-
-var gallery = document.querySelector('#gallery');
-var largeImg = gallery.querySelector('#largeImg');
-var thumbs = gallery.querySelector('#thumbs');
-
-galleryDefault();
-
-gallery.addEventListener('click', gallerySort);
+});
